3320581ec3f6e05302016837f726c84acc9d953a
manonaidinian <manonaidinian@gmail.com>
2022-05-31 15:07:44 +0800

    adding review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  RATING = (0..10)
+  validates :comment, :booking_rating, presence: true
+  validates :bike_rating, :owner_rating, :booking_rating, inclusion: { in: RATING }
 end
