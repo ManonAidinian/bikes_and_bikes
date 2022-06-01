fc0f913e057bfb2f8713ee737b6b25199ed561a7
manonaidinian <manonaidinian@gmail.com>
2022-06-01 11:30:14 +0800

    adding bike controller + index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,10 @@ Rails.application.routes.draw do
 
   # I doubt that we need the following.
   # Maybe we can do that right from the bikes#show, after clicking "Book now!"
-  get '/booking/new',             to: 'booking#new'
-  post '/booking/new',            to: 'booking#create'
-  get '/booking/:id/new_review',  to: 'booking#new_review'
-  post '/booking/:id/new_review', to: 'booking#create_review'
+  get '/bookings/new',             to: 'bookings#new'
+  post '/bookings/new',            to: 'bookings#create'
+  get '/bookings/:id/new_review',  to: 'bookings#new_review'
+  post '/bookings/:id/new_review', to: 'bookings#create_review'
 
   get '/styleguide', to: 'pages#styleguide'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
